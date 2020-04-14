95ba65f99eef0914b61f55321c1231c493104514
Viswa527 <Viswa527@github.com>
2020-04-14 16:29:49 +0530

    [Viswa]Refactor.method names are modified

---
+++ Usecase_6/src/main/java/com/Usecase_6.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Usecase_6 {
+    public Boolean rule2(String string)
+    {
+        return  string.length()>=8;
+    }
+}
---
+++ Usecase_6/src/test/java/Usecase_6Testing.java
@@ -0,0 +1,38 @@
+import com.Usecase_6;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Usecase_6Testing {
+    @Test
+    public void testcase_Length_Validation_IsTrue()
+    {
+        Usecase_6 usecase = new Usecase_6();
+        String string = "hello welcome";
+        Boolean result = usecase.rule2(string);
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void testcase_Length_Validation2_IsTrue()
+    {
+        Usecase_6 usecase = new Usecase_6();
+        String string = "welcometojava  ";
+        Boolean result = usecase.rule2(string);
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void testcase_Length_Validation3_IsTrue()
+    {
+        Usecase_6 usecase = new Usecase_6();
+        String string = "welcome";
+        Boolean result = usecase.rule2(string);
+        Assert.assertEquals(false,result);
+    }
+    @Test
+    public void testcase_Rule2_Validation4_IsTrue()
+    {
+        Usecase_6 usecase = new Usecase_6();
+        String string = "Hello";
+        Boolean result = usecase.rule2(string);
+        Assert.assertEquals(false,result);
+    }
+}
